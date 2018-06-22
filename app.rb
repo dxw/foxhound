d8ce7653bf2a1bd04d6a29d537d53d348f2595df
Tom Hipkin <tomhipkin@gmail.com>
2018-06-22 17:30:23 +0100

    (feature): Payment links are created when the root path is visited
    * We considered where to put this. Either when the view was requested or adding a redirection step that could calculate and forward the request on with the @next_url but chose the former so that we could deliver something in a day.
    * When a temporary event notice application is submitted we would like to generate a new payment link with GOV.UK Pay that was unique to that application. This would ensure we don’t create more payments with GOV.UK than are required. We could see when developing this we had lots of ‘dead’ transaction in the admin part of GOV.UK because links were generated on every page load.

--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
 require 'sinatra'
 require 'haml'
 require 'govuk_pay_api_client'
+require 'securerandom'
 require 'pry'
 require 'byebug'
 
 get '/' do
+  @next_url = create_payment
   haml :pay_now
 end
 get '/success' do
   haml :success
+end
+def create_payment
+  reference = SecureRandom.hex(4)
+  GovukPayApiClient::CreatePayment.call(
+    OpenStruct.new(
+      description: 'Scarfolk council temporary event notice',
+      govpay_reference: reference,
+      amount: 2100,
+    ),
+    "#{base_url}/success/#{reference}",
+  ).next_url
 end
