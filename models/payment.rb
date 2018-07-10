1dda99fa5fc7f969debb26b66e7399f860a83941
Robbie Paul <robbie.paul@dxw.com>
2018-07-10 15:37:14 +0100

    (refactor) Move GOV Pay API calls in to the Payment model
    The two are closely related and it makes sense to keep them together.

--- models/payment.rb
+++ models/payment.rb
@@ -1,3 +1,5 @@
+require 'govuk_pay_api_client'
+
 class Payment < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :govpay_reference
@@ -5,5 +7,34 @@ class Payment < ActiveRecord::Base
 
   def successful?
     status == 'success'
+  end
+
+  def pending?
+    status == 'pending'
+  end
+
+  def failed?
+    status == 'failed'
+  end
+
+  def update_govpay_status
+    return if pending?
+    GovukPayApiClient::GetStatus.call(self).tap do |govpay_response|
+      update(status: govpay_response.status)
+    end
+  end
+
+  def create_govpay_payment(return_url:)
+    return unless pending?
+    GovukPayApiClient::CreatePayment.call(
+      self,
+      return_url
+    ).tap do |govpay_response|
+      update(
+        govpay_payment_id: govpay_response.payment_id,
+        govpay_url: govpay_response.next_url,
+        status: :created
+      )
+    end
   end
 end
