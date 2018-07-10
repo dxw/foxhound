2a3ca1402011dcffb41feba133d8ee4c540bdb0b
Robbie Paul <robbie.paul@dxw.com>
2018-07-10 17:37:06 +0100

    (feature) Configure our app to use Sinatra Asset Pipeline

--- app.rb
+++ app.rb
@@ -7,31 +7,16 @@ require 'securerandom'
 require 'pry'
 require 'byebug'
 require 'excon'
-require 'sprockets'
-require 'uglifier'
-require 'sass'
+require 'sinatra/asset_pipeline'
 
 Dir['./models/*.rb'].each { |file| require file }
 
 class Foxhound < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :database_file, 'config/database.yml'
-  # initialize new sprockets environment
-  set :environment, Sprockets::Environment.new
 
-  # append assets paths
-  environment.append_path 'assets/stylesheets'
-  environment.append_path 'assets/javascripts'
-
-  # compress assets
-  environment.js_compressor  = :uglify
-  environment.css_compressor = :scss
-
-  # get assets
-  get '/assets/*' do
-    env['PATH_INFO'].sub!('/assets', '')
-    settings.environment.call(env)
-  end
+  set :assets_paths, %w(assets/javascripts assets/stylesheets)
+  register Sinatra::AssetPipeline
 
   get '/' do
     @pcn = PenaltyChargeNotice.new
