106437aebae3fdba5b744cfa8d40445ada340989
Robbie Paul <robbie.paul@dxw.com>
2018-07-20 15:45:13 +0100

    (feature) Add issued_at to penalty charge notice table
    We want to randomly assign an issued at date so we can decided if we
    want to discount the total amount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_111856) do
+ActiveRecord::Schema.define(version: 2018_07_20_142203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_07_20_111856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "charge_type"
+    t.date "issued_at"
     t.index ["payment_id"], name: "index_penalty_charge_notices_on_payment_id"
   end
 
