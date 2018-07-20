3a707d233daaafacd546e154f3a788f0ac44d9eb
Robbie Paul <robbie.paul@dxw.com>
2018-07-20 12:55:34 +0100

    (feature) Add penalty charge notice type to payment
    Randomly set the type of penalty charge using the after_initialize
    callback.
    
    Add a `charge_amount` method that uses the type to get the amount of the
    charge.
    
    Convert the type to human readable string for GovPay description and PCN
    summary page.

--- models/penalty_charge_notice.rb
+++ models/penalty_charge_notice.rb
@@ -1,6 +1,24 @@
 class PenaltyChargeNotice < ActiveRecord::Base
+  after_initialize :set_charge_type
+
   validates_presence_of :pcn_number
   validates_presence_of :vehicle_registration_mark
 
   belongs_to :payment
+
+  CHARGES = {
+    parking_meter_expired: 60_00,
+    no_parking_permit: 110_00,
+    driving_in_bus_lane: 130_00
+  }.freeze
+
+  def charge_amount
+    CHARGES[charge_type.to_sym]
+  end
+
+  private
+
+  def set_charge_type
+    self.charge_type = CHARGES.keys.sample
+  end
 end
